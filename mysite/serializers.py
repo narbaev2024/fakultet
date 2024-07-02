84443ca7a1245091533942904f8f2cbca97cf6f9
narbaev2024 <165474675+narbaev2024@users.noreply.github.com>
2024-07-02 19:12:12 +0500

    Update serializers.py

--- mysite/serializers.py
+++ mysite/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from .models import  Fakultet, Professor, Student, Course, Cabinet, Raspisanie, Zapis_na_course,Dz, Sdacha_dz
+from .models import  Department, Professor, Student, Cours
 
-class FakultetSerialzer(serializers.ModelSerializer):
+class DepartmentSerialzer(serializers.ModelSerializer):
     class Meta:
-        model = Fakultet
+        model = Department
         fields = '__all__'
 
 
@@ -17,37 +17,7 @@ class StudentSerialzer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = '__all__'
-
-class CourseSerialzer(serializers.ModelSerializer):
+class CoursSerialzer(serializers.ModelSerializer):
     class Meta:
-        model = Course
+        model = Cours
         fields = '__all__'
-
-class CabinetSerialzer(serializers.ModelSerializer):
-    class Meta:
-        model = Cabinet
-        fields = '__all__'
-
-
-class RaspisanieSerialzer(serializers.ModelSerializer):
-    class Meta:
-        model = Raspisanie
-        fields = '__all__'
-
-
-class Zapis_na_courseSerialzer(serializers.ModelSerializer):
-    class Meta:
-        model = Zapis_na_course
-        fields = '__all__'
-
-
-class DzSerialzer(serializers.ModelSerializer):
-    class Meta:
-        model = Dz
-        fields = '__all__'
-
-
-class Sdacha_dzSerialzer(serializers.ModelSerializer):
-    class Meta:
-        model = Sdacha_dz
-        fields = ['assigment', 'student', 'submission_date', 'grade', 'feedback']
